9ce2a7ae7f8e97425b74c4b8d46037a379bf9e44
Arman Hossain Somoy <61237257+armansomoy@users.noreply.github.com>
2024-03-30 23:31:54 +0600

    error

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # simple-crud-server
+
+// importnat link: https://www.mongodb.com/docs/drivers/node/current/usage-examples/insertOne/
--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-const express = require('express');
-const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
-const port = process.env.PORT ||  5000;
+const port = process.env.PORT || 5000;
 
+// middleware
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
 // asamoy3
 // bMaWylwHDX2qwQMD
 
-
-
-const uri = "mongodb+srv://asamoy3:bMaWylwHDX2qwQMD@cluster0.u7telnd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri =
+  "mongodb+srv://asamoy3:bMaWylwHDX2qwQMD@cluster0.u7telnd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -20,16 +20,29 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const database = client.db("usersDB");
+    const userCollection = database.collection("users");
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      console.log("new user", user);
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     await client.close();
@@ -37,12 +50,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-app.get('/', (req, res) => {
-    res.send('simple curd is running')
-})
+app.get("/", (req, res) => {
+  res.send("simple curd is running");
+});
 
 app.listen(port, () => {
-    console.log(`new port: ${port}`)
-})
-
+  console.log(`new port: ${port}`);
+});
